9ee900d0f736e8f9c78f6c60baa8d8863f2fb988
Anton Andrianov <ant.e.andrianov@gmail.com>
2020-10-20 23:24:23 +0300

    Первая стилизация кнопок

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 let html = document.documentElement;
-            let themeButtons = document.querySelectorAll("[data-set-theme]");
+let themeButtons = document.querySelectorAll("[data-set-theme]");
 
-            // Если тема была ранее задана, присваиваем ее значение
-            if (localStorage.getItem("theme") != null) {
-                document.documentElement.setAttribute(
-                    "data-theme",
-                    localStorage.getItem("theme")
-                );
-            }
+// Если тема была ранее задана, присваиваем ее значение
+if (localStorage.getItem("theme") != null) {
+    document.documentElement.setAttribute(
+        "data-theme",
+        localStorage.getItem("theme")
+    );
+}
 
-            // Клик на одну из кнопок меняет тему
-            themeButtons.forEach((button) => {
-                let theme = button.dataset.setTheme;
+// Клик на одну из кнопок меняет тему
+themeButtons.forEach((button) => {
+    let theme = button.dataset.setTheme;
 
-                button.addEventListener("click", () => {
-                    html.dataset.theme = theme;
-                    localStorage.setItem("theme", theme);
-                });
+    button.addEventListener("click", () => {
+        html.dataset.theme = theme;
+        localStorage.setItem("theme", theme);
+    });
 
-                // Принудительная очистка локального хранилища
-                // localStorage.clear();
-            });
+    // Принудительная очистка локального хранилища
+    // localStorage.clear();
+});
 
-            // Вывод в консоль содержимого локального хранилища
-            console.log(localStorage);
+// Вывод в консоль содержимого локального хранилища
+console.log(localStorage);
