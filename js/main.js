715d20aed2bf27f426853712f29b5e02c5c65b04
Anton Andrianov <ant.e.andrianov@gmail.com>
2020-10-20 18:15:49 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let html = document.documentElement;
+            let themeButtons = document.querySelectorAll("[data-set-theme]");
+
+            // Если тема была ранее задана, присваиваем ее значение
+            if (localStorage.getItem("theme") != null) {
+                document.documentElement.setAttribute(
+                    "data-theme",
+                    localStorage.getItem("theme")
+                );
+            }
+
+            // Клик на одну из кнопок меняет тему
+            themeButtons.forEach((button) => {
+                let theme = button.dataset.setTheme;
+
+                button.addEventListener("click", () => {
+                    html.dataset.theme = theme;
+                    localStorage.setItem("theme", theme);
+                });
+
+                // Принудительная очистка локального хранилища
+                // localStorage.clear();
+            });
+
+            // Вывод в консоль содержимого локального хранилища
+            console.log(localStorage);
