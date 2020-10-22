dd2f0db9ed08fb9060c223de6b8d446f3494fe1b
Anton Andrianov <ant.e.andrianov@gmail.com>
2020-10-22 15:04:25 +0300

    Рефакторинг кнопок и скрипта
    Кнопки сделал с помощью инпутов, чтобы не писал лишний скрипт для выбранных состояний. Группа радиокнопок умеет это делать из коробки. Сделал по-умолчанию выбранной тему авто при первой загрузке сайта.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,27 @@
 let html = document.documentElement;
 let themeButtons = document.querySelectorAll("[data-set-theme]");
 
-// Если тема была ранее задана, присваиваем ее значение
-if (localStorage.getItem("theme") != null) {
-    document.documentElement.setAttribute(
-        "data-theme",
-        localStorage.getItem("theme")
-    );
+// Назначаем тему странице
+if (localStorage.getItem("theme") == null) {
+    // Если первый раз на сайте, то ставим по-умолчанию авто
+    document.documentElement.setAttribute("data-theme", "auto");
+}
+else {
+    // Иначе запоминаем выбранную тему в локальное хранилище
+    document.documentElement.setAttribute("data-theme", localStorage.getItem("theme"));
 }
 
 // Клик на одну из кнопок меняет тему
 themeButtons.forEach((button) => {
     let theme = button.dataset.setTheme;
-
     button.addEventListener("click", () => {
         html.dataset.theme = theme;
         localStorage.setItem("theme", theme);
     });
-
-    // Принудительная очистка локального хранилища
-    // localStorage.clear();
 });
+
+// Принудительная очистка локального хранилища
+// localStorage.clear();
 
 // Вывод в консоль содержимого локального хранилища
 console.log(localStorage);
